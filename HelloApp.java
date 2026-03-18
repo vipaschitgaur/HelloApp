f8d2fb8b3a5f1905e19c94db6d49b42b4f67ce08
ReallyyTrash <navyaattri6@gmail.com>
2026-03-18 10:30:53 +0530

    Ternary operator for the uc3

--- HelloApp.java
+++ HelloApp.java
@@ -1,9 +1,5 @@
 public class HelloApp {
     public static void main(String[] args) {
-        String word = "World";
-        if (args.length > 0) {
-            word = args[0];
-        }
-        System.out.println("Hello, " + word + "!");
+        args.length > 0 ? System.out.println("Hello, " + args[0] + "!") : System.out.println("Hello, World!");
     }
 }
