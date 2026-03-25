6e43ba890061cd11d3d92b91294a6dac33952639
Vipaschit Gaur <vipaschitattri@gmail.com>
2026-03-25 09:39:43 +0530

    Updatd and fixed uc4
    handles multiple arguments and gives a greeting depending on the length of the argument input

--- HelloApp.java
+++ HelloApp.java
@@ -1,9 +1,16 @@
 public class HelloApp {
     public static void main(String[] args) {
+        StringBuilder nameBuilder = new StringBuilder();
+        String name = "World";
         if (args.length > 0) {
-            System.out.println("Hello, " + args[0] + "!");
-        } else {
-            System.out.println("Hello, World!");
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+            name = nameBuilder.toString();
         }
+        System.out.println("Hello, " + name + "!");
     }
 }
