7a33c0c061cdffdbff54c7fa942c734d6a550fb7
Vipaschit Gaur <vipaschitattri@gmail.com>
2026-04-04 00:49:40 +0530

    Enhacned for loop
    iterating over each word not the index of the input arguments

--- HelloApp.java
+++ HelloApp.java
@@ -1,16 +1,18 @@
 public class HelloApp {
     public static void main(String[] args) {
         StringBuilder nameBuilder = new StringBuilder();
-        String name = "World";
+        // String name = "World";
         if (args.length > 0) {
-            for (int i = 0; i < args.length; i++) {
-                nameBuilder.append(args[i]);
-                if (i < args.length - 1) {
-                    nameBuilder.append(", ");
-                }
+
+            for (String name: args) {
+                nameBuilder.append(name).append(", ");
             }
-            name = nameBuilder.toString();
+                nameBuilder.delete(nameBuilder.length() - 2, nameBuilder.length());
+
         }
-        System.out.println("Hello, " + name + "!");
+        else{
+            nameBuilder.append("World");
+        }
+        System.out.println("Hello, " + nameBuilder.toString().trim() + "!");
     }
 }
