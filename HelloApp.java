c144ef7e703ffd6db7817a5d06e2ba17ea703929
Vipaschit Gaur <vipaschitattri@gmail.com>
2026-04-06 04:41:08 +0530

    using string.join the native funciton to join the naems using the delimiter

--- HelloApp.java
+++ HelloApp.java
@@ -1,18 +1,11 @@
 public class HelloApp {
     public static void main(String[] args) {
-        StringBuilder nameBuilder = new StringBuilder();
-        // String name = "World";
-        boolean first = true;
+        String names = "World";
 
-        for (String name : args) {
-            nameBuilder.append (name);
-            nameBuilder.append (", ");
+        if (args.length > 0) {
+            names = String.join(", ", args);
         }
 
-        if (nameBuilder.length() == 0) {
-            nameBuilder.append("World");
-        }
-        String names = nameBuilder.substring(0, nameBuilder.length() - 2);
         System.out.println("Hello, " + names + "!");
-}
+    }
 }
